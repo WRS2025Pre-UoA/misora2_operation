35b49f4f20e025e02b9dc735d71328cadda26129
rui323 <ruiiwata837@gmail.com>
2025-04-20 11:25:53 +0900

    add comment header file

--- include/misora2_operation/operator_gui_component.hpp
+++ include/misora2_operation/operator_gui_component.hpp
@@ -57,37 +57,37 @@ public:
     int bulb_state_count = 0;
 
     //　受け取ったメッセージを格納-------------------------------------
-    std_msgs::msg::String id, result_data;
-    cv::Mat temporary_image;
-    std::unique_ptr<cv::Mat> result_image;
+    std_msgs::msg::String id, result_data;// 確認ノードへ報告を行う時に必要なid 結果を格納
+    cv::Mat temporary_image;// sensor_msgsで送られてくるので一時的にcv::Matへ
+    std::unique_ptr<cv::Mat> result_image;// 確認ノードへ送信する画像
 
     std::vector<std::string> trigger_list = {"pressure", "qr", "cracks", "metal_loss"};
     explicit MisoraGUI(const rclcpp::NodeOptions &options);
     MisoraGUI() : MisoraGUI(rclcpp::NodeOptions{}) {}
 
 private:
-    cv ::Mat setup();
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg);
-    void timer_callback();
-    void mouse_click_callback(const geometry_msgs::msg::Point::SharedPtr msg);
-    void rewriteImage(cv::Point sp, cv::Point ep, std::string text, int btn_W, int btn_H, cv::Scalar color) const;
-    void process(std::string topic_name);
+    cv ::Mat setup();// ボタン画面生成生成
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg);// 検出結果をうけとった時に行う処理関数
+    void timer_callback();// 定期的にボタン画像を流す
+    void mouse_click_callback(const geometry_msgs::msg::Point::SharedPtr msg);// ボタン画面にクリックした時の座標をもとに行う処理関数
+    void rewriteImage(cv::Point sp, cv::Point ep, std::string text, int btn_W, int btn_H, cv::Scalar color) const;// 指定したボタンの色、表示内容を変更
+    void process(std::string topic_name);// クリックしたボタンに対応した処理を行う関数
 
-    rclcpp::Publisher<MyAdaptedType>::SharedPtr publish_gui_;
-    rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr click_;
-    rclcpp::TimerBase::SharedPtr view_;
+    rclcpp::Publisher<MyAdaptedType>::SharedPtr publish_gui_;// ボタン画面を流すpublisher
+    rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr click_;// ボタンクリック座標を受け取るsubscriber
+    rclcpp::TimerBase::SharedPtr view_;// ボタン画面を定期的に流すタイマー
 
     // std::map<std::string, rclcpp::Time> button_press_times_;
     
-    std::map<std::string, rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr> bool_triggers_;
-    std::map<std::string, rclcpp::Subscription<std_msgs::msg::String>::SharedPtr> receive_data_;
-    std::map<std::string, rclcpp::Subscription<MyAdaptedType>::SharedPtr> receive_image_;
+    std::map<std::string, rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr> bool_triggers_;// 連続処理信号
+    std::map<std::string, rclcpp::Subscription<std_msgs::msg::String>::SharedPtr> receive_data_;// 検出結果をうけとる　
+    std::map<std::string, rclcpp::Subscription<MyAdaptedType>::SharedPtr> receive_image_;// 検出画像をうけとる
 
     rclcpp::Subscription<MyAdaptedType>::SharedPtr receive_raw_image_;// MISORAから来る生画像
 
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr dt_qr_publisher_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr dt_data_publisher_;
-    rclcpp::Publisher<MyAdaptedType>::SharedPtr dt_image_publisher_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr dt_qr_publisher_;// 確認ノードへidを送る
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr dt_data_publisher_;// 確認ノードへ検出結果を送る
+    rclcpp::Publisher<MyAdaptedType>::SharedPtr dt_image_publisher_;// 確認ノードへ検出画像を送る
     
 };
 
